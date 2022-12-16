db4b0490de472f05f7e6ac0a176b3b2db1dbbbd6
Giles Knap <giles.knap@diamond.ac.uk>
2022-12-16 16:33:42 +0000

    update docs and move venv out of Dockerfile

--- demo/docker_test.py
+++ demo/docker_test.py
@@ -2,7 +2,7 @@
 Example docker API client.
 
 This is a demonstrator of the use of the docker API from inside the
-devcontainer provided by dev-u22-workspace
+devcontainer provided by dev-u22
 
 To try this, start the podman_service listening on docker API socket,
 install python docker, run this script and then exec a shell in the
