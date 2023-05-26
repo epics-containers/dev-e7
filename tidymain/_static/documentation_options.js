59e8bf3e812bd86441682fc36c71ea21df3a06f9
gilesknap <gilesknap@users.noreply.github.com>
2023-05-26 06:46:26 +0000

    deploy: 7f2e1a0ea2200c9777a2bce1cb3bfa2d1fb71e76

--- tidymain/_static/documentation_options.js
+++ tidymain/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '23.5.1-1-gfb00552',
+    VERSION: '23.5.1-2-g7f2e1a0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
